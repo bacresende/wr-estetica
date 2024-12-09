65c41f414d6af7fcd03a765f55f61a0d80f21ff4
Bruno <bacresende@gmail.com>
2024-12-09 12:11:04 -0300

    updates

--- src/app/modules/pages/usuario/components/info-gerais/info-gerais.component.ts
+++ src/app/modules/pages/usuario/components/info-gerais/info-gerais.component.ts
@@ -1,16 +1,24 @@
 import { Component, Input } from '@angular/core';
 import { Usuario } from '../../../../models/usuario.model';
+import { FieldsetModule } from 'primeng/fieldset';
+import { CadastroUsuario } from '../../../../models/cadastro-usuario.model';
+import { CommonModule } from '@angular/common';
+import { getStatusFuncaoPorExtenso } from '../../../../../shared/obter-status';
 
 @Component({
   selector: 'app-info-gerais',
   standalone: true,
-  imports: [],
+  imports: [FieldsetModule, CommonModule],
   templateUrl: './info-gerais.component.html',
   styleUrl: './info-gerais.component.css'
 })
 export class InfoGeraisComponent {
 
   @Input()
-  public usuario: Usuario | undefined;
+  public usuarioRep: CadastroUsuario | undefined;
+
+  public getStatusFuncaoPorExtenso(status: string){
+    return getStatusFuncaoPorExtenso(status);
+  }
 
 }
